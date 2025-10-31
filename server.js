86be45f88bfdce1b69fbef7cbd6543c1323ffa1f
Mukesh689 <muthumukesh689@gmail.com>
2025-10-31 23:33:50 +0530

    Update CORS origin to new frontend URL

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: ["https://<your-frontend-name>.onrender.com"],
+  origin: ["https://gamecd0689.netlify.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
